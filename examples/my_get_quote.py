0f73189637e9bbbbd86602de5ed1d7c6eff9295e
dgl-dev <les@dunawaygroupllc.com>
2021-12-12 16:52:57 -0500

    Forked tda-api; added my_get_quote.py and my_get_tx_hist.py

---
+++ examples/my_get_quote.py
@@ -0,0 +1,32 @@
+from urllib.request import urlopen
+
+import atexit
+import datetime
+import dateutil
+import sys
+import tda
+
+API_KEY = 'FON1HLNGRN0KOVR6UDTCF4RPEMPYIXOB@AMER.OAUTHAP'
+REDIRECT_URI = 'http://localhost:8080/'
+TOKEN_PATH = '//access_token'
+
+
+def make_webdriver():
+    # Import selenium here because it's slow to import
+    from selenium import webdriver
+
+    driver = webdriver.Chrome()
+    atexit.register(lambda: driver.quit())
+    return driver
+
+
+# Create a new client
+client = tda.auth.easy_client(
+    API_KEY,
+    REDIRECT_URI,
+    TOKEN_PATH,
+    make_webdriver)
+
+
+r = client.get_quote("IBM")
+print(r.json())
---
+++ examples/my_get_tx_hist.py
@@ -0,0 +1,41 @@
+import json
+from urllib.request import urlopen
+
+import atexit
+import datetime
+import dateutil
+import sys
+import tda
+
+API_KEY = 'FON1HLNGRN0KOVR6UDTCF4RPEMPYIXOB@AMER.OAUTHAP'
+REDIRECT_URI = 'http://localhost:8080/'
+TOKEN_PATH = '//access_token'
+
+
+def make_webdriver():
+    # Import selenium here because it's slow to import
+    from selenium import webdriver
+
+    driver = webdriver.Chrome()
+    atexit.register(lambda: driver.quit())
+    return driver
+
+
+# Create a new client
+client = tda.auth.easy_client(
+    API_KEY,
+    REDIRECT_URI,
+    TOKEN_PATH,
+    make_webdriver)
+
+std = datetime.date(2021,8,1)
+etd = datetime.date(2021,11,30)
+tx_type = client.Transactions.TransactionType.TRADE
+
+r = client.get_transactions(490673362,
+                            transaction_type=tx_type,
+                            start_date=std, end_date=etd)
+#print(json.dumps(r.json()))
+for tx in r.json():
+    print(json.dumps(tx, indent=1))
+    print("\n +++++++++++ \n")
